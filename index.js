3f1381fedd2c9183e0d725b60c2d01baaabfeda4
Manuel Pils <manuel@Manuels-MacBook-Pro.local>
2021-04-30 10:54:21 +0200

    DEV-923 ADD no-console rule to base config

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   rules: {
     'implicit-arrow-linebreak': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'object-curly-newline': ['error', {
       consistent: true,
